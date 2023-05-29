274743834fa1afb73373ec6b5604d65683481c2f
Jun <grandy201@gmail.com>
2023-05-29 16:26:18 +0700

    feat: simplify, upgrade lib

--- go.mod
+++ go.mod
@@ -1,22 +1,20 @@
 module gitlab.com/with-junbach/go-modules/config
 
-go 1.17
+go 1.20
 
 require (
-	github.com/knadh/koanf v1.3.3
-	github.com/stretchr/testify v1.7.0
+	github.com/knadh/koanf/parsers/yaml v0.1.0
+	github.com/knadh/koanf/providers/env v0.1.0
+	github.com/knadh/koanf/providers/file v0.1.0
+	github.com/knadh/koanf/v2 v2.0.1
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
-	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/knadh/koanf/maps v0.1.1 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
-	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
-	github.com/pelletier/go-toml v1.7.0 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/sys v0.0.0-20200331124033-c3d80250170d // indirect
-	gopkg.in/yaml.v2 v2.2.8 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+	golang.org/x/sys v0.0.0-20220908164124-27713097b956 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- loader.go
+++ loader.go
@@ -2,44 +2,39 @@ package config
 
 import (
 	"fmt"
-	"github.com/knadh/koanf"
-	"github.com/knadh/koanf/parsers/json"
 	"github.com/knadh/koanf/parsers/yaml"
 	"github.com/knadh/koanf/providers/env"
 	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/v2"
+
 	"path/filepath"
 	"reflect"
 	"strings"
 )
 
-func LoadConfig(configObject interface{}, envPrefix string, configFiles ...string) error {
+func Load(configObject any, envPrefix string, configFiles ...string) error {
 	if pt := reflect.TypeOf(configObject).Kind(); pt != reflect.Ptr {
 		return fmt.Errorf("invalid input config object, should be pointer instead of %v", pt)
 	}
 
-	loader := koanf.New(".")
+	inst := koanf.New(".")
+	parser := yaml.Parser()
 	for _, filename := range configFiles {
-		var parser koanf.Parser
-		switch ext := strings.ToLower(filepath.Ext(filename)); ext {
-		case ".yml", ".yaml":
-			parser = yaml.Parser()
-		case ".json":
-			parser = json.Parser()
-		default:
-			return fmt.Errorf("invalid config file: %s", filename)
+		if ext := strings.ToLower(filepath.Ext(filename)); ext != ".yml" && ext != ".yaml" {
+			return fmt.Errorf("invalid config file extension, only YAML supported: %s", filename)
 		}
 
-		if err := loader.Load(file.Provider(filename), parser); err != nil {
+		if err := inst.Load(file.Provider(filename), parser); err != nil {
 			return fmt.Errorf("failed to load config file %s: %v", filename, err)
 		}
 	}
 
-	if err := loader.Load(env.Provider(envPrefix, ".", func(s string) string {
+	if err := inst.Load(env.Provider(envPrefix, ".", func(s string) string {
 		return strings.Replace(strings.ToLower(strings.TrimPrefix(s, envPrefix)), "_", ".", -1)
 	}), nil); err != nil {
 		return fmt.Errorf("failed to load config from environment: %v", err)
 	}
-	if err := loader.UnmarshalWithConf("", configObject, koanf.UnmarshalConf{Tag: "map"}); err != nil {
+	if err := inst.UnmarshalWithConf("", configObject, koanf.UnmarshalConf{Tag: "config"}); err != nil {
 		return fmt.Errorf("failed to unmarshal config into object: %v", err)
 	}
 	return nil
